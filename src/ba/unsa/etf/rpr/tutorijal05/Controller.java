1a75573f9afcd6e10c6becb4c02520ccda955866
bfks <sedamdana7321>
2018-11-11 21:34:29 +0100

    implementirana funckija product

--- src/ba/unsa/etf/rpr/tutorijal05/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal05/Controller.java
@@ -1,5 +1,112 @@
 package ba.unsa.etf.rpr.tutorijal05;
 
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    public TextField textBox;
+    double input=0;
+    double a=0;
+    double b=0;
+    boolean isA=false;
+    boolean isB=false;
+    int izbor=0;
+    double rez=0;
+    double lastNum=0;
+    double proiz=1;
+    boolean kucano=false;
+
+    boolean prvi=false;
+
+    public void nineClick(ActionEvent actionEvent) {
+        input=10*input+9;
+        postavi(input);
+        textBox.setText(input+"");
+    }
+    public void eightClick(ActionEvent actionEvent) {
+        input=10*input+8;
+        postavi(input);
+        textBox.setText(input+"");
+    }
+    public void sevenClick(ActionEvent actionEvent) {
+        input=10*input+7;
+        postavi(input);
+        textBox.setText(input+"");
+    }
+    public void sixClick(ActionEvent actionEvent) {
+        input=10*input+6;
+        postavi(input);
+        textBox.setText(input+"");
+    }
+    public void fiveClick(ActionEvent actionEvent) {
+        input=10*input+5;
+        postavi(input);
+        textBox.setText(input+"");
+    }
+    public void fourClick(ActionEvent actionEvent) {
+        input=10*input+4;
+        postavi(input);
+        textBox.setText(input+"");
+    }
+    public void threeClick(ActionEvent actionEvent) {
+        input=10*input+3;
+        postavi(input);
+        textBox.setText(input+"");
+    }
+    public void twoClick(ActionEvent actionEvent) {
+        input=10*input+2;
+        postavi(input);
+        textBox.setText(input+"");
+    }
+    public void oneClick(ActionEvent actionEvent) {
+        input=10*input+1;
+        postavi(input);
+        textBox.setText(input+"");
+    }
+    public void zeroClick(ActionEvent actionEvent) {
+        input=10*input+0;
+        postavi(input);
+        textBox.setText(input+"");
+    }
+    private void postavi(double input){
+        kucano=true;
+        prvi=true;
+        lastNum=input;
+    }
+
+    public void procClick(ActionEvent actionEvent) {
+
+    }
+    public void minusClick(ActionEvent actionEvent) {
+
+    }
+    public void plusClick(ActionEvent actionEvent) {
+
+    }
+    public void productClick(ActionEvent actionEvent) {
+        if(kucano){
+            if(prvi && !isA){rez=input; isA=true; textBox.setText(rez+""); input=0; return;}
+            rez*=input;
+            textBox.setText(rez+"");
+        }
+        kucano=false;
+        input=0;
+        izbor=3;
+    }
+    public void divideClick(ActionEvent actionEvent) {
+
+    }
+    public void equalsClick(ActionEvent actionEvent) {
+        product();
+    }
+    public void product(){
+        rez*=lastNum;
+        textBox.setText(rez+"");
+        input=0;
+        kucano=false;
+        prvi=false;
+        isA=false;
+    }
 
 }
